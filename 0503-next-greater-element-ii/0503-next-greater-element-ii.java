a394925e7cb33a9844cd3779ef80b135d0ba1202
Burugula Raghavendra <23211a0547@bvrit.ac.in>
2026-07-06 14:52:12 +0530

    Time: 17 ms (23.52%), Space: 48.7 MB (22.81%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n=nums.length;
         int ngr[]= new int[n];
-        int [] num= new int[2*n];
-        for(int i=0;i<2*n;i++){
-            num[i]=nums[i%n];
-        }
+        // int [] num= new int[2*n];
+        // for(int i=0;i<2*n;i++){
+        //     num[i]=nums[i%n];
+        // }
         Stack<Integer> s= new Stack<>();
         for(int i=2*n-1;i>=0;i--){
             while(!s.isEmpty() && s.peek()<=nums[i%n]){
