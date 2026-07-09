006d6d06878bd8691966290b4d361a65976a5d3c
Burugula Raghavendra <23211a0547@bvrit.ac.in>
2026-07-09 10:41:53 +0530

    Time: 16 ms (39.59%), Space: 47.7 MB (48.36%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i:nums){
+            hm.put(i,hm.getOrDefault(i,0)+1);
+        }
+        ArrayList<Integer> al = new ArrayList<>(hm.keySet());
+        Collections.sort(al,(a,b)->hm.get(b)-hm.get(a));
+        int res[] =new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=al.get(i);
+        }
+        return res;
+
+    }
+}
