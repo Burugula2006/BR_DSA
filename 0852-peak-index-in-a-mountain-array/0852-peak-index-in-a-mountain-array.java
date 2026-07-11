2685d593fe2b2a427e2e07f578f4ed587a8077b2
Burugula Raghavendra <23211a0547@bvrit.ac.in>
2026-07-11 22:03:01 +0530

    Time: 0 ms (100%), Space: 80.6 MB (8.83%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,37 +1,20 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         
-             int n = arr.length;
-        int[] result = new int[n];
-        Stack<Integer> stack = new Stack<>();
-
-        // Initialize with default values
-        Arrays.fill(result, -1);
-
-        // Traverse the array from right to left
-        for (int i = n - 1; i >= 0; i--) {
-            // Pop elements smaller than or equal to current element
-            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
-                stack.pop();
-            }
-
-            // Top of the stack is our next greater element
-            if (!stack.isEmpty()) {
-                result[i] = stack.peek();
-            }
-
-            // Always push the current element onto the stack
-            stack.push(arr[i]);
-        }
+        int n = arr.length;
         int ans=-1;
-        for(int i=0;i<n;i++){
-            if(result[i]>ans){
-                ans=i;
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(arr[m]<arr[m+1]){
+                l=m+1;
+            }
+            else {
+                r=m;
             }
         }
-        return ans+1;
-
-
+        return l;
        
     }
 }
