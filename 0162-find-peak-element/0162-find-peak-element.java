a68aec3df6e8dbad9eed8efecc04ec9e916161e5
Burugula Raghavendra <23211a0547@bvrit.ac.in>
2026-07-11 22:34:08 +0530

    Time: 0 ms (100%), Space: 44.3 MB (18.08%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,19 +1,22 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int n=nums.length;
-        int l=0;
-        int r=nums.length-1;
+        
        
+        
+        int l=0;
+        int r=n-1;
         while(l<r){
             int m=l+(r-l)/2;
-            if( nums[m]>nums[m+1]){
-                r=m;
-            }
-            
-            else{
+            if(nums[m]<nums[m+1]){
                 l=m+1;
             }
+            else {
+                r=m;
+            }
         }
+        
+       
         return l;
     }
 }
