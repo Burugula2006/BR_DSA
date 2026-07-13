a9bb2fc792c8d02d7745ae58fad30a5621c525a3
Burugula Raghavendra <23211a0547@bvrit.ac.in>
2026-07-13 12:35:01 +0530

    Time: 24 ms (94.14%), Space: 92.1 MB (26.67%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,29 +1,50 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        int l=1;
-        int r=0;
-        for(int i:candies){
-            r=Math.max(r,i);
+    //     int l=1;
+    //     int r=0;
+    //     for(int i:candies){
+    //         r=Math.max(r,i);
+    //     }
+    //     int ans=0;
+    //     while(l<=r){
+    //         int mid=l+(r-l)/2;
+    //         boolean res=f(candies,mid,k);
+    //         if(res){
+    //             ans=mid;
+    //             l=mid+1;
+    //         }
+    //         else{
+    //             r=mid-1;
+    //         }
+    //     }
+    // return ans;
+    // }
+    // public boolean f(int [] candies,int mid ,long k){
+    //     long c=0;
+    //     for(int i:candies){
+    //         c=c+(i/mid);
+    //     }
+    //     return c>=k;
+    // }
+    long total = 0, co = 0;
+        int left = 1, right = 0, mid = 0;
+        for (int count : candies) {
+            total += count;
+            right = Math.max(right, count);
         }
-        int ans=0;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            boolean res=f(candies,mid,k);
-            if(res){
-                ans=mid;
-                l=mid+1;
+        if (total < k) return 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            co = 0;
+            for (int count : candies) {
+                co += count / mid;
             }
-            else{
-                r=mid-1;
+            if (co >= k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-    return ans;
-    }
-    public boolean f(int [] candies,int mid ,long k){
-        long c=0;
-        for(int i:candies){
-            c=c+(i/mid);
-        }
-        return c>=k;
-    }
+        return right;
+}
 }
