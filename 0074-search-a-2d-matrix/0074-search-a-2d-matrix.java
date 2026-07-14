670d5295edf47218953eedcdf8f2be1cb47fbc56
Burugula Raghavendra <23211a0547@bvrit.ac.in>
2026-07-14 20:12:46 +0530

    Time: 0 ms (100%), Space: 43.9 MB (43.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,26 +2,38 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int k=0;
-        int arr[]= new int[m*n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-            {
-                arr[k++]=matrix[i][j];
-            }
-        }
-        int l=0;
-        int r=m*n-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(arr[mid]==target)return true;
-            else if(arr[mid]<target){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
-            }
+        // int k=0;
+        // int arr[]= new int[m*n];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         arr[k++]=matrix[i][j];
+        //     }
+        // }
+        // int l=0;
+        // int r=m*n-1;
+        // while(l<=r){
+        //     int mid=l+(r-l)/2;
+        //     if(arr[mid]==target)return true;
+        //     else if(arr[mid]<target){
+        //         l=mid+1;
+        //     }
+        //     else{
+        //         r=mid-1;
+        //     }
+        // }
+        // return false;
+        int left=0;
+        int right=m*n-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int row=mid/n;
+            int col=mid%n;
+                if(matrix[row][col]==target)return true;
+            else if(matrix[row][col]>target)right=mid-1;
+            else left=mid+1;;
         }
         return false;
+        
     }
 }
