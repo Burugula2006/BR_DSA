efddacd1510d13d55ff67efb160726b97760da75
Burugula Raghavendra <23211a0547@bvrit.ac.in>
2026-07-14 21:00:23 +0530

    Time: 3 ms (89.68%), Space: 48.1 MB (64.58%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -2,19 +2,14 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m=matrix.length;
         int n=matrix[0].length;
-       for(int i=0;i<m;i++){
         int l=0;
         int r=n-1;
-        int arr[]=matrix[i];
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(arr[mid]==target)return true;
-            else if(arr[mid]<target)l=mid+1;
-            else{
-                r=mid-1;
-            }
+        while(l<m && r>=0){
+            if(matrix[l][r]==target)return true;
+            else if(matrix[l][r]<target)l++;
+            else r--;
+
         }
-       }
-       return false;
+        return false;
     }
 }
